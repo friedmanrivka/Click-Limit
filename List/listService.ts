526805d99b3d8a379748988d415bb2ffc64c0229
friedmanrivka <hand1c26@mbjcomp.org.il>
2024-06-18 18:16:49 +0300

    fix en erorr please now pull the changes and from now everyone will implement the function she has to do,the function of get all was not good so implemement it again or fix it when you can do it

--- List/listService.ts
+++ List/listService.ts
@@ -1,12 +1,12 @@
 
 import ListDal from './listDal'
-import { List } from "../utils/type";
+
 
 export default class ListService{
     constructor(private listDal: ListDal) {}
-
-    public async getLists(filter: any): Promise<List[]> {
-        return this.listDal.getLists(filter);
-    }
 }
+    // public async getLists(filter: any): Promise<List[]> {
+    //     return this.listDal.getLists(filter);
+    // }
+
 
--- app.ts
+++ app.ts
@@ -4,6 +4,7 @@ import ListDal from "./list/listDal";
 import ListService from "./list/listService";
 // import ListApi from './list/listApi';
 import DBConnect from "./utils/db-connect";
+import ListApi from "./list/ListApi";
 const PORT=8080;
 const LIST_API_ROUTE='/api/list';
 export default class App{
@@ -15,7 +16,7 @@ export default class App{
         await this.dbConn.init();
         const listDal=new ListDal(this.dbConn);
         const listService=new ListService(listDal);
-        const listApi=new ListApi();
+        const listApi=new ListApi(listService);
         this.app=express();
         this.app.use(LIST_API_ROUTE,listApi.router);
         this.app.listen(PORT,()=>{
